41d680a61cf7144114a504e78a4171a5893f6d37
Alexander F <florean.alexander@gmail.com>
2024-09-30 22:31:11 +0200

    clean up

--- init.lua
+++ init.lua
@@ -290,33 +290,6 @@ require('lazy').setup({
     end,
   },
 
-  -- Magma-nvim: plugin for running code interactively with Jupyter
-  --{
-  --  'dccsillag/magma-nvim',
-  --  version = '*',
-  --  lazy = false,
-  --  -- workaround while debugging other stuff
-  --  ft = { 'ipynb' },
-  --  keys = {
-  --    { '<leader>mi', '<cmd>MagmaInit<CR>', desc = 'This command initializes a runtime for the current buffer.' },
-  --    { '<leader>mo', '<cmd>MagmaEvaluateOperator<CR>', desc = 'Evaluate the text given by some operator.' },
-  --    { '<leader>ml', '<cmd>MagmaEvaluateLine<CR>', desc = 'Evaluate the current line.' },
-  --    { '<leader>mv', '<cmd>MagmaEvaluateVisual<CR>', desc = 'Evaluate the selected text.' },
-  --    { '<leader>mc', '<cmd>MagmaEvaluateOperator<CR>', desc = 'Reevaluate the currently selected cell.' },
-  --    { '<leader>mr', '<cmd>MagmaRestart!<CR>', desc = 'Shuts down and restarts the current kernel.' },
-  --    { '<leader>mx', '<cmd>MagmaInterrupt<CR>', desc = 'Interrupts the currently running cell and does nothing if not cell is running.' },
-  --  },
-  --},
-  -- Jupynum: plugin for running jupyter notebooks on nvim
-  --{
-  --  'kiyoon/jupynium.nvim',
-  --build = 'pip3 install --user .',
-  --   build = 'conda run --no-capture-output -n scai-ext-exp pip install .',
-  -- enabled = vim.fn.isdirectory(vim.fn.expand "~/miniconda3/envs/jupynium"),
-  --},
-  --'rcarriga/nvim-notify', -- optional
-  --'stevearc/dressing.nvim', -- optional, UI for :JupyniumKernelSelect
-
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
   -- This is often very useful to both group configuration, as well as handle
